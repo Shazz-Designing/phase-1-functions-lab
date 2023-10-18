b72de6ca21b8985bb90a6571f8348f000e040db7
sharon <sapot@safaricom.co.ke>
2023-10-18 06:56:15 +0300

    Add distance travelled in feet

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function distanceFromHqInBlocks(pickupLocation) {
     return Math.abs(pickupLocation - 42); 
-  }
+}
   
 
 function distanceFromHqInFeet(pickupLocation) {
   const blocks = distanceFromHqInBlocks(pickupLocation);
   return blocks * 264;
 }
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264; 
+}
+  
 

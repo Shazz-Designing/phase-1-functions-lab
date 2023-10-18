07848201b97d55a59fcaa9c2ee0eb2465184c5b7
sharon <sapot@safaricom.co.ke>
2023-10-18 06:59:31 +0300

    Calculate Fare

--- index.js
+++ index.js
@@ -12,4 +12,17 @@ function distanceTravelledInFeet(startBlock, endBlock) {
     return Math.abs(startBlock - endBlock) * 264; 
 }
   
-
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+  
+    if (distance <= 400) {
+      return 0; // First 400 feet are free
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot beyond the first 400 feet
+    } else if (distance < 2500) {
+      return 25; // Flat fare for distances between 2000 and 2500 feet
+    } else {
+      return 'cannot travel that far'; // Distance is over 2500 feet
+    }
+  }
+  
